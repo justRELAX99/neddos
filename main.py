e3ca087447aca19e3d410c1a2921e86cb257a2ba
justRELAX99 <etozherelax@gmail.com>
2020-03-19 18:07:41 +0300

    version 3

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
-import requests
-from selenium import webdriver
-from requests_html import HTMLSession
-from requests_html import requests
-import time
+from datetime import datetime
+from requests_html import *
 from memory_profiler import memory_usage
-import io
-
 from random import choice
 from multiprocessing import Pool
 
-def get_html(url,user_agent,proxy):
+
+def auth_request(url,user_agent):
+    session = HTMLSession()
+    
     header={
         'User-Agent':user_agent
         }
-    proxies={
-        'http':proxy
-        }
-    response = requests.get(url,headers=header,proxies=proxies)
 
-    return response.text
+    data={
+        'login':'monstr541@yandex.ru',
+        'password':'bzr7quj9913'
+    }
+    response=session.post(url,headers=header,data=data)
+    print(response.cookies)
+    response=session.get('https://vk.com/feed')
+    html=response.html
+    html.render()
+    To_File(html.html,'html4.txt')
+    print('---------------------------------')
+    print(response.cookies)
+    print('---------------------------------')
+    session.close()
 
-def get_request_html(url,user_agent,proxy):
+def get_request_html(url,user_agent,proxy,session):
     header={
         'User-Agent':user_agent
         }
@@ -30,66 +37,40 @@ def get_request_html(url,user_agent,proxy):
 
     script = """
     document.body.removeChild(document.getElementById('masthead'))"""
-    session = HTMLSession()
-    response=session.request('get',url,headers=header,proxies=proxies).html
-    response.render(script=script)
-    session.close()
-    return response.html
+    response=session.request('get',url,headers=header,proxies=proxies,timeout=5)
+    html=response.html
+    html.render(script=script)
+    return html.html
 
-def get_html_selenium(url):
-        options=webdriver.ChromeOptions()
-        options.add_argument("--headless")
-        script = """
-        document.body.removeChild(document.getElementById('masthead'))"""  
-        driver=webdriver.Chrome('chromedriver.exe', options=options)
-        driver.get(url)
-        driver.execute_script(script)
-        html =driver.page_source
-        driver.quit()
-        return html
-
-def To_File_TXT(html,name):
+def To_File(html,name):
         f=open(name,'w', encoding='utf-8')
         f.write(html)
         f.close()
 
-
-def get_status_code(url,proxy,user_agent):
-    header={
-        'User-Agent':user_agent
-        }
-
-    proxies={
-        'http':proxy
-        }
-
-    try:
-        response=requests.get(url,headers=header,proxies=proxies,timeout=10)
-        print(response.content)
-        return response.status_code
-    except:
-        return (proxy,'-Ошибка подключения')
-
 def main():
-    url='https://python-scripts.com/'#тестовый
+    start=datetime.now()
+
+    url='https://auth.mail.ru/cgi-bin/auth?from=splash'#тестовый
     name_user_agent='user_agent.txt'
     name_proxy_file='proxy_file.txt'
-
+    responses=[]
     user_agents=open(name_user_agent).read().split('\n')
     proxies=open(name_proxy_file).read().split('\n')
-
-    user_agent=choice(user_agents)
-    proxy=choice(proxies)
-    To_File_TXT (get_html(url,user_agent,proxy),'html.html')
-    #To_File_TXT(get_html_selenium(url),'html2.txt')
-    #To_File_TXT(get_request_html(url,user_agent,proxy),'html3.html')
+    
+    # session = HTMLSession()
     # for proxy in proxies:
     #     user_agent=choice(user_agents)
-    #     status_code=get_status_code(url,proxy,user_agent)
-    #     if(status_code!=200):
-    #         print(status_code)
-    #     break
-    print(memory_usage())
+    #     response=get_request_html(url,user_agent,proxy,session)
+    #     responses.append(response)
+    # else:
+    #     session.close()
+    #print(responses)
+    user_agent=choice(user_agents)
+    auth_request(url,user_agent)
+    end=datetime.now()
+    print(memory_usage(),'---end')
+    total=end-start
+    print(total)
 
 if __name__ == "__main__":
     main()
