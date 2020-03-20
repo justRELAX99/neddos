1a09be3ccf071a6487b00352a655a190c124ccce
justRELAX99 <etozherelax@gmail.com>
2020-03-21 01:24:16 +0300

    threads version

--- main.py
+++ main.py
@@ -1,46 +1,67 @@
 from datetime import datetime
-from requests_html import *
 from memory_profiler import memory_usage
+
+from requests_html import *
 from random import choice
-from multiprocessing import Pool
+from bs4 import BeautifulSoup
 
+from concurrent.futures import ProcessPoolExecutor
+from itertools import repeat
 
-def auth_request(url,user_agent):
+def auth_request(url,user_agent,proxy):
     session = HTMLSession()
-    
-    header={
-        'User-Agent':user_agent
-        }
 
-    data={
-        'login':'monstr541@yandex.ru',
-        'password':'bzr7quj9913'
+    headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
     }
-    response=session.post(url,headers=header,data=data)
-    print(response.cookies)
-    response=session.get('https://vk.com/feed')
-    html=response.html
-    html.render()
-    To_File(html.html,'html4.txt')
-    print('---------------------------------')
-    print(response.cookies)
-    print('---------------------------------')
-    session.close()
-
-def get_request_html(url,user_agent,proxy,session):
-    header={
-        'User-Agent':user_agent
-        }
     proxies={
         'http':proxy
-        }
+    }
+    response = session.get(url, headers=headers,proxies=proxies,timeout=5)
+    soup=BeautifulSoup(response.text,'lxml')
+    post_url=soup.find(id='quick_login_form').get('action')
+    post_elems=soup.find(id='quick_login_form').find_all('input')#парсим lg_h т.к. он меняется 
+    for elem in post_elems:
+        if(elem.get('name')=='lg_h'):
+            lg_h=elem.get('value')
+            break
+    data=[
+    ('act', 'login'), 
+    ('role', 'al_frame'), 
+    ('expire', ''), 
+    ('_origin', 'https://vk.com'), 
+    ('ip_h', 'e9d7291c1045299089'), 
+    ('lg_h', lg_h),
+    ('email', 'monstr541@yandex.ru'), 
+    ('pass', 'bzr7quj991331AA')
+    ]
 
-    script = """
-    document.body.removeChild(document.getElementById('masthead'))"""
-    response=session.request('get',url,headers=header,proxies=proxies,timeout=5)
-    html=response.html
-    html.render(script=script)
-    return html.html
+    response = session.post(post_url, data=data,proxies=proxies,timeout=5)
+    cookie=response.cookies
+    session.close()
+    return cookie
+
+
+def auth_cookie_request(url,user_agent,proxy,cookie):
+    session = HTMLSession()
+    headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36'
+    }
+    proxies={
+        'http':proxy
+    }
+    # script = """
+    # document.body.removeChild(document.getElementById('masthead'))"""
+    response = session.get(url+'feed', headers=headers,proxies=proxies,cookies=cookie,timeout=5)
+    #html=response.html
+    #html.render(script=script)
+    soup=BeautifulSoup(response.text,'lxml')
+    name=soup.find(class_='top_profile_name').text
+    session.close()
+    return name
+
+def print123(url,user_agent,proxy,cookie):
+    return('url-',url,';user_agent-',user_agent,';proxy-',proxy,';cookie-',cookie)
 
 def To_File(html,name):
         f=open(name,'w', encoding='utf-8')
@@ -50,23 +71,19 @@ def To_File(html,name):
 def main():
     start=datetime.now()
 
-    url='https://auth.mail.ru/cgi-bin/auth?from=splash'#тестовый
+    url='https://vk.com/'#тестовый
     name_user_agent='user_agent.txt'
     name_proxy_file='proxy_file.txt'
-    responses=[]
     user_agents=open(name_user_agent).read().split('\n')
     proxies=open(name_proxy_file).read().split('\n')
-    
-    # session = HTMLSession()
-    # for proxy in proxies:
-    #     user_agent=choice(user_agents)
-    #     response=get_request_html(url,user_agent,proxy,session)
-    #     responses.append(response)
-    # else:
-    #     session.close()
-    #print(responses)
     user_agent=choice(user_agents)
-    auth_request(url,user_agent)
+    cookie=auth_request(url,user_agent,choice(proxies))
+    
+    with ThreadPoolExecutor(len(proxies)) as executor:
+        names=list(executor.map(auth_cookie_request,repeat(url),repeat(user_agent),proxies,repeat(cookie)))
+
+    print(names)
+
     end=datetime.now()
     print(memory_usage(),'---end')
     total=end-start
