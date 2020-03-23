22ac562731ea29badc9a1db8fb230895276e98ba
justRELAX99 <etozherelax@gmail.com>
2020-03-23 03:02:48 +0300

    add cookies auth

--- main.py
+++ main.py
@@ -1,63 +1,82 @@
-import requests
 from selenium import webdriver
-from requests_html import HTMLSession
-from requests_html import requests
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
 from datetime import datetime
 from memory_profiler import memory_usage
-import io
 
 from random import choice
 from multiprocessing import Pool
 
 
-def get_html_selenium3(url,user_agent,proxy):
+def get_cookies_selenium(url,user_agent,proxy):
     options=webdriver.ChromeOptions()
     options.add_argument('--proxy-server-%s'%proxy)
-    options.add_experimental_option('w3c', False)
-    #options.add_argument("--headless")
+    options.add_argument('user-agent='+user_agent)
+    options.add_argument("--headless")
     script = """
     document.getElementById('index_email').value = 'monstr541@yandex.ru'
     document.getElementById('index_pass').value = 'bzr7quj991331AA'
     document.getElementById('index_login_button').click()"""
     driver=webdriver.Chrome('chromedriver.exe', options=options)
-    driver.get(url)
-    driver.execute_script(script)
-    cookies =driver.get_cookies()
-    driver.quit()
-    return cookies
-
-def get_html_selenium2(url,user_agent,proxy,cookies):
-    options=webdriver.ChromeOptions()
-    options.add_argument('--proxy-server-%s'%proxy)
-    options.add_experimental_option('w3c', False)
-    #options.add_argument("--headless")
-    driver=webdriver.Chrome('chromedriver.exe', options=options)
-    driver.get(url)
-
-    for cookie in cookies:
+    driver.set_page_load_timeout(10)
+    driver.set_script_timeout(10)
+    try:
+        driver.get(url)
+    except:
+        print('The request failed')
+        driver.quit()
+        return 0
+    try:
+        driver.execute_script(script)
+    except:
+        print('The script failed')
+        driver.quit()
+        return 0
+    try:
+        element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "ui_rmenu_news_list")))
+    except:
+        print('Element not found')
+        driver.quit()
+        return 0
+    finally:
+        cookies =driver.get_cookies()
+        driver.quit()
         for cookie in cookies:
             if 'expiry' in cookie:
-                cookie['expiry'] = int(cookie['expiry'])
-        driver.add_cookie(cookie)
+                cookie['expiry'] = int(cookie['expiry']*1000)
+        return cookies
 
-    print(driver.get_cookies())
-    time.sleep(6)
-    driver.quit()
-
-
-def get_html_selenium(url,user_agent,proxy):
-        options=webdriver.ChromeOptions()
-        options.add_argument('--proxy-server-%s'%proxy)
-        #options.add_argument("--headless")
-        script = """
-        document.body.removeChild(document.getElementById('masthead'))"""  
-        driver=webdriver.Chrome('chromedriver.exe', options=options)
+def auth_with_cookies_selenium(url,user_agent,proxy,cookies):
+    options=webdriver.ChromeOptions()
+    options.add_argument('--proxy-server-%s'%proxy)
+    options.add_argument('user-agent='+user_agent)
+    options.add_argument("--headless")
+    driver=webdriver.Chrome('chromedriver.exe', options=options)
+    driver.set_page_load_timeout(10)
+    driver.set_script_timeout(10)
+    try:
         driver.get(url)
-        driver.execute_script(script)
-        html =driver.page_source
+    except:
         driver.quit()
-        return html
+        print('The request failed')
+        return 0
+    for cookie in cookies:
+        driver.add_cookie(cookie)
+    driver.refresh()
+    
+    try:
+        element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "ui_rmenu_news_list")))
+    except:
+        print('Element not found')
+        driver.quit()
+        return 0
+    finally:
+        print('Performed')
+        driver.quit()
+        return 1
+
 
 def To_File(html,name):
         f=open(name,'w', encoding='utf-8')
@@ -66,7 +85,7 @@ def To_File(html,name):
 
 def main():
     start=datetime.now()
-    url='https://python-scripts.com/'#тестовый
+    url='https://vk.com/'#тестовый
     name_user_agent='user_agent.txt'
     name_proxy_file='proxy_file.txt'
 
@@ -75,11 +94,11 @@ def main():
 
     user_agent=choice(user_agents)
     proxy=choice(proxies)
-    for proxy in proxies:
-        user_agent=choice(user_agents)
-        html=get_html_selenium(url,user_agent,proxy)
-        To_File(html,'html.html')
-        break
+    
+    cookies=get_cookies_selenium(url,user_agent,proxy)
+    if(cookies!=0):
+        auth_with_cookies_selenium(url,user_agent,proxy,cookies)
+
     print(memory_usage())
     end=datetime.now()
     total=end-start
