c8aa4b305d24da723f129316ef106fa6b1abe1ae
justRELAX99 <etozherelax@gmail.com>
2020-03-25 18:17:45 +0300

    ferst commit

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from selenium import webdriver
+import pprint
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+import asyncio
 from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
 from multiprocessing import Pool
 
@@ -47,43 +48,6 @@ def get_cookies_selenium(url,user_agent,proxy,script_for_auth):
                 cookie['expiry'] = int(cookie['expiry']*1000)
         return cookies
 
-def auth_with_cookies_selenium(url,user_agent,proxy,cookies,script_for_selenium):
-    options=webdriver.ChromeOptions()
-    options.add_argument('--proxy-server-%s'%proxy)
-    options.add_argument('user-agent='+user_agent)
-    options.add_argument("--headless")
-    driver=webdriver.Chrome('chromedriver.exe', options=options)
-    driver.set_page_load_timeout(10)
-    driver.set_script_timeout(10)
-    try:
-        driver.get(url)
-    except:
-        driver.quit()
-        print('The request failed')
-        return 0
-    
-    for cookie in cookies:
-        driver.add_cookie(cookie)
-    driver.refresh()
-    
-    try:
-        element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "ui_rmenu_news_list")))
-    except:
-        print('Element not found')
-        driver.quit()
-        return 0
-    finally:
-        try:
-            driver.execute_script(script_for_selenium)
-            print('Performed')
-            driver.quit()
-            return 1
-        except:
-            print('The script failed')
-            driver.quit()
-            return 0
-
-
 def test_proxy_auth(url,user_agent,proxy):
     options=webdriver.ChromeOptions()
     options.add_argument('--proxy-server-%s'%proxy)
@@ -107,16 +71,6 @@ def parting(xs, parts):
     part_len = ceil(len(xs)/parts)
     return [xs[part_len*k:part_len*(k+1)] for k in range(parts)]
 
-def make_threads(data):
-    urls=data[0]
-    user_agents=data[1]
-    proxies=data[2]
-    cookies=data[3]
-    scripts_for_selenium=[4]
-    with ThreadPoolExecutor(len(proxies)) as executor:
-        answer=list(executor.map(auth_with_cookies_selenium,urls,user_agents,proxies,cookies,scripts_for_selenium))
-        return answer
-
 def get_proxy_by_numbers(name_proxy_file,numbers_proxy):
     all_proxies=open(name_proxy_file).read().split('\n')
     proxies=[]
@@ -132,7 +86,11 @@ def main(url,numbers_proxy,script_for_auth,script_for_selenium):
 
     user_agents=open(name_user_agent).read().split('\n')
 
-    proxies=get_proxy_by_numbers(name_proxy_file,numbers_proxy)
+    if(len(numbers_proxy)!=0):
+        proxies=get_proxy_by_numbers(name_proxy_file,numbers_proxy)
+    else:
+        proxies=open(name_proxy_file).read().split('\n')
+
     user_agent=choice(user_agents)
     proxy=choice(proxies)
 
@@ -143,29 +101,67 @@ def main(url,numbers_proxy,script_for_auth,script_for_selenium):
     #test_proxy_auth('https://2ip.ru/',user_agent,proxy)
     
     #cookies=get_cookies_selenium(url,user_agent,proxy)
-    processes_count=os.cpu_count()
-
-    # if(cookies!=0):
-    #     parting_url=parting([url]*len(proxies),processes_count)
-    #     parting_user_agent=parting([user_agent]*len(proxies),processes_count)
-    #     parting_proxies=parting(proxies,processes_count)
-    #     parting_cookie=parting([cookies]*len(proxies),processes_count)
-    #     parting_script_for_selenium=parting([script_for_selenium]*len(proxies),processes_count)
-    #     data=[]
-    #     for i in range(0,processes_count):
-    #         data.append([parting_url[i],parting_user_agent[i],parting_proxies[i],parting_cookie[i],parting_script_for_selenium[i]])
-
-    #     with Pool(processes_count) as p:
-    #         answer=list(p.map(make_threads,data))
     
-    #print(answer)
+    with ThreadPoolExecutor(len(proxies)) as executor:
+
+        def scrape(proxy, *, loop):
+            nonlocal url
+            nonlocal user_agent
+            nonlocal cookies
+            nonlocal script_for_selenium
+            loop.run_in_executor(executor,auth_with_cookies_selenium, url,user_agent,proxy,cookies,script_for_selenium)
+
+        def auth_with_cookies_selenium(url,user_agent,proxy,cookies,script_for_selenium):
+            options=webdriver.ChromeOptions()
+            options.add_argument('--proxy-server-%s'%proxy)
+            options.add_argument('user-agent='+user_agent)
+            options.add_argument("--headless")
+            driver=webdriver.Chrome('chromedriver.exe', options=options)
+            driver.set_page_load_timeout(10)
+            driver.set_script_timeout(10)
+            try:
+                driver.get(url)
+            except:
+                driver.quit()
+                print('The request failed')
+                return 0
+            
+            for cookie in cookies:
+                driver.add_cookie(cookie)
+            driver.refresh()
+            
+            try:
+                element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "ui_rmenu_news_list")))
+            except:
+                print('Element not found')
+                driver.quit()
+                return 0
+            finally:
+                try:
+                    driver.execute_script(script_for_selenium)
+                    print('Performed')
+                    driver.quit()
+                    return 1
+                except:
+                    print('The script failed')
+                    driver.quit()
+                    return 0
+
+        loop = asyncio.get_event_loop()
+        for proxy in proxies:
+            scrape(proxy, loop=loop)
+
+        answer=loop.run_until_complete(asyncio.gather(*asyncio.all_tasks(loop)))
+
+
+    pprint.pprint(answer)
     end=datetime.now()
     total=end-start
     print(total)
 
 if __name__ == "__main__":
     url='https://vk.com/'#тестовый
-    numbers_proxy=[1,3,5]
+    numbers_proxy=[]
     script_for_auth="""
     document.getElementById('index_email').value = 'monstr541@yandex.ru'
     document.getElementById('index_pass').value = 'bzr7quj991331AA'
